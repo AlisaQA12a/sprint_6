857767f26430cf1eb2cb0c96dd412d03a94676fa
Alisa Cherepanina <alisastewardess@yandex.ru>
2025-03-12 05:20:03 +0300

    reset repo

--- URL.py
+++
@@ -1,3 +0,0 @@
-DZEN_URL = "https://dzen.ru/?yredirect=true"  #DZEN
-MAINPAGE_URL = "https://qa-scooter.praktikum-services.ru/"  #главная страница
-ORDERPAGE_URL = "https://qa-scooter.praktikum-services.ru/order"  #страница заказа
